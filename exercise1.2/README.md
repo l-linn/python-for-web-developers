3d137344711d2c4c9563748b5f43a350e5070c50
lin <lin7cb@outlook.com>
2024-05-28 19:31:16 +0100

    update screenshots

---
+++ exercise1.2/README.md
@@ -0,0 +1,9 @@
+### Data strcture for the recipes:
+
+I chose dictionary as the data structure for recipe_1/single recipe because it is easier to label the elements with Key and Value pairs in this senario. Each recipe will have different blocks of information (name, cooking time, ingredients and possibly measures and steps as well)and with key and value pairs it's easy to group them together.
+
+We will need to amend the data later so it's better to use mutable data structure.
+
+### Data strcture for all recipes:
+
+I chose using a list to store all recipes. List is used to store multiple items in a single variable, which makes sense that in this case we have many recepies stored in one 'recipe book'. It's orded and changable which is easy to track down each recipe and modify it.
---
+++ exercise1.2/recipes.py
@@ -0,0 +1,38 @@
+recipe_1 = {
+  'name': 'Tea',
+  'cooking_time_in_minutes' : 5,
+  'ingredients':['Tea leaves','Suger','Water']
+}
+
+recipe_2 = { 
+  'name': 'Banana Pancakes',
+  'cooking_time_in_minutes' : 15,
+  'ingredients':['Bananas','Eggs','Flour','Baking powder','Salt','Butter','Maple syrup','Fresh Berries']
+
+}
+
+recipe_3 = {
+  'name': 'Tacos',
+  'cooking_time_in_minutes' : 20,
+  'ingredients':['Chicken','Taco seasoning','Taco shells','Lettuce','Tomatoes','Cheese','Sour cream','Salsa']
+}
+
+recipe_4 = {
+  'name': 'Omelette',
+  'cooking_time_in_minutes' : 10,
+  'ingredients':['Eggs','Milk','Salt','Pepper','Cheese','Ham','Onions','Mushrooms']
+}
+
+recipe_5 = {
+  'name': 'Chicken Stir-Fry',
+  'cooking_time_in_minutes' : 25,
+  'ingredients':['Chicken','Soy sauce','Hoisin sause','Sesame oil','Bell pepper','Broccoli','Garlic','Ginger','Rice']
+}
+
+
+all_recipes = [recipe_1, recipe_2, recipe_3, recipe_4, recipe_5]
+
+for i in all_recipes:
+  i_ingredients = i.get('ingredients')
+  print(i_ingredients)
+
