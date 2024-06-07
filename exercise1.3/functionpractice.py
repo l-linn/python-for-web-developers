7eaf5b78e55ada1f551c243f9c319b4f8979bd43
lin <lin7cb@outlook.com>
2024-06-07 23:15:47 +0100

    Update functionpractice.py

--- exercise1.3/functionpractice.py
+++ exercise1.3/functionpractice.py
@@ -6,3 +6,9 @@ guest_list('Craig', 46)
 guest_list() #will return default arguments
 #Guest Name: unknown
 #Age: unknown
+
+user_destination = input('Where do you want to go?\n') #start a new line using \n
+if user_destination == 'New York' or user_destination == 'London' or user_destination == 'Tokyo':
+  print('Enjoy your stay in ' + user_destination + '!')
+else: 
+  print('Oops, that destination is not currently available.')
