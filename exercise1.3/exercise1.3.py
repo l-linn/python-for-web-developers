0956eedeead32c0f8d176d3e6850516668d99c2b
lin <lin7cb@outlook.com>
2024-06-08 08:58:45 +0100

    Update exercise1.3.py

--- exercise1.3/exercise1.3.py
+++ exercise1.3/exercise1.3.py
@@ -4,21 +4,59 @@ ingredients_list = []
 def take_recipe():
   name = input('Please give your recipe a name: ')
   cooking_time = input('How long is the cooking time? In minutes please: ')
-  
-  ingredients = []
-  ingredients = input('Please list out the ingredients needed: ')
+
+  ingredients = [] #create an empty list then add user input into the list as list item
+  print('Please list out the ingredients (type \'done\' when finished)') #skip quotes - adding backslash bedore hte single quotation 
+  while True:
+    ingredient = input('- ')
+    if ingredient.lower() == 'done':
+      break
+    ingredients.append(ingredient)
 
   recipe = {
     'name': name,
     'cooking_time': cooking_time,
     'ingredients': ingredients
     }
-  
-  print(recipe)
-  return recipe
+  return recipe #need to return the variable to save it
 
 n = int(input('How many recipes would you like to entre? '))
 
 for i in range(n):
   recipe = take_recipe()
+
+  for ingredient in recipe['ingredients']:
+    if ingredient not in ingredients_list:
+      ingredients_list.append(ingredient)
+  
+  recipes_list.append(recipe)
+  print(recipes_list)
+
+
+for recipe in recipes_list:
+  cooking_time = int(recipe['cooking_time'])
+  num_of_ingredients = len(recipe['ingredients'])
+
+  if cooking_time < 10 and num_of_ingredients < 4:
+    difficulty = 'Easy'
+  elif cooking_time < 10 and num_of_ingredients >= 4:
+    difficulty = 'Medium'
+  elif cooking_time >= 10 and num_of_ingredients >= 4:
+    difficulty = 'Hard'
+  else:
+    print('Hmm, something is not right.')
+
+  recipe['difficulty'] = difficulty #add difficulty as a key to the recipe dict
+
+print('\nRecipes List:')
+for recipe in recipes_list:
+    print(f"Recipe Name: {recipe['name']}") #need to use double quotes for printing dict's key value pairs
+    print(f"Cooking Time (min): {recipe['cooking_time']}")
+    print("Ingredients:\n- " + '\n- '.join(recipe['ingredients']))#use join method to join the values to one string
+    print(f"Difficulty Level: {recipe['difficulty']}")
+
+ingredients_list.sort()
+print('Ingredients Available Across All Recipes\n- - - - - - - - - - - - - - - - - -')
+for ingredient in ingredients_list:
+  print(ingredient)
 
