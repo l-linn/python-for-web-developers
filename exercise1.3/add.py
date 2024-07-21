b37ac51296fe4d3a726d2b29dcc6c289b22e97d3
lin <lin7cb@outlook.com>
2024-07-21 20:01:17 +0100

    Update add.py

--- exercise1.3/add.py
+++ exercise1.3/add.py
@@ -1,9 +1,9 @@
-a = input('Enter a number: ')
-b = input('Enter another number to be added to the first: ')
+a = input("Enter a number: ")
+b = input("Enter another number to be added to the first: ")
 a_int = int(a)
 b_int = int(b)
-print('The sum of these numbers is', a_int + b_int)
-#or
-c = int(input('Enter a number: '))
-d = int(input('Enter another number to be added to the first: '))
-print('The sum of these numbers is ' + str(c + d))
+print("The sum of these numbers is", a_int + b_int)
+# or
+c = int(input("Enter a number: "))
+d = int(input("Enter another number to be added to the first: "))
+print("The sum of these numbers is " + str(c + d))
