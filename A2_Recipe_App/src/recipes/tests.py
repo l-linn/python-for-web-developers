7a50e79af9c16d40b5e666920af234a8667b7396
lin <lin7cb@outlook.com>
2024-08-02 23:13:09 +0100

    test

--- A2_Recipe_App/src/recipes/tests.py
+++ A2_Recipe_App/src/recipes/tests.py
@@ -20,11 +20,8 @@ class RecipeTestCase(TestCase):
         self.assertEqual(field_label, "name")
 
     def test_recipe_name_max_length(self):
-        # Get a cook object to test
         recipe = Recipe.objects.get(id=1)
-        # Get the metadata for the 'author_name' field and use it to query its max_length
         max_length = recipe._meta.get_field("name").max_length
-        # Compare the value to the expected result i.e. 120
         self.assertEqual(max_length, 50)
 
 
